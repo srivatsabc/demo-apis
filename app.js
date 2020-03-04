be2f362a15f3173703139d40f58a9f4251458a77
srivatsabc <srivatsabc@gmail.com>
2020-03-04 08:51:32 +0530

    Commit

--- app.js
+++ app.js
@@ -14,6 +14,16 @@ app.get('/hello/:name', function (req, res) {
   res.status(200).json({'Hello Good Boy': req.params.name.toUpperCase(), 'Date': new Date()});
 })
 
+app.get('/images/:name', function (req, res) {
+  console.log("name: " + req.params.name.toUpperCase());
+  res.status(200).json({'Hello images': req.params.name.toUpperCase(), 'Date': new Date()});
+})
+
+app.get('/videos/:name', function (req, res) {
+  console.log("name: " + req.params.name.toUpperCase());
+  res.status(200).json({'Hello videos': req.params.name.toUpperCase(), 'Date': new Date()});
+})
+
 var server = app.listen(8080, function () {
    console.log("Example app listening at http on tcp/8080")
 })
